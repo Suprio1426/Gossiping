2c8c7c48f12a97cd4816023ea389def2e48c00ef
Suprio Nath <129674948+Suprio1426@users.noreply.github.com>
2025-08-01 01:17:42 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,22 +1,22 @@
- import axios from 'axios';
+import axios from "axios";
 
- const axiosInstance = axios.create({
-    baseURL: "https://gossipingai.vercel.app/"
-,
- });
-    axiosInstance.interceptors.request.use(
+const axiosInstance = axios.create({
+  baseURL: "https://gossipingai-backend.vercel.app", 
+});
+
+axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // ✅ always latest
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
   (error) => Promise.reject(error)
 );
 
-
-
- export default axiosInstance;
+export default axiosInstance;
 
  //THIS IS THE AXIOS INSTANCE THAT WE WILL USE TO MAKE REQUESTS TO THE BACKEND... 
